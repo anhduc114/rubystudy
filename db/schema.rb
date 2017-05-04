e45133cfd376099c535688ee75a40828e76ba463
Duc Do <jimraynor1104@gmail.com>
2017-05-04 14:30:23 +0000

    Make a basic User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503134925) do
+ActiveRecord::Schema.define(version: 20170504141616) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20170503134925) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
