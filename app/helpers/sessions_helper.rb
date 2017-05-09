f0ff2c68ca03fe1a3e5b1a9bb51da382a1724605
Duc Do <jimraynor1104@gmail.com>
2017-05-09 03:13:19 +0000

    advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,21 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+    # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
       # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -16,6 +31,7 @@ module SessionsHelper
   
     # Logs out the current user.
   def log_out
+         forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504142602) do
+ActiveRecord::Schema.define(version: 20170509030021) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -26,8 +25,8 @@ ActiveRecord::Schema.define(version: 20170504142602) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
