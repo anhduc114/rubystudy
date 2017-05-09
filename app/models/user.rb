717d96fa106e02524f42dc32d5f14dc9429ec388
Duc Do <jimraynor1104@gmail.com>
2017-05-09 02:39:37 +0000

    Use SSL and the Puma webserver in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
       before_save { self.email = email.downcase }
+       
+       has_secure_password
+       attr_accessor :email, :name, :password, :password_confirmation
+ validates_length_of :password, :in => 6..20, :on => :create
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
                       validates :password, presence: true, length: { minimum: 6 }
+                      
 end
